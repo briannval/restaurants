e70cb6a25c6295652b3001340f8e77cbe3662539
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-10-20 16:59:54 -0700

    Dockerized spring boot

--- restaurants-be/src/test/java/com/brian/restaurants/RestaurantsApplicationTests.java
+++ restaurants-be/src/test/java/com/brian/restaurants/RestaurantsApplicationTests.java
@@ -1,13 +1,8 @@
 package com.brian.restaurants;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestaurantsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
